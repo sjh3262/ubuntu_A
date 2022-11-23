7147b18df01af7efc661a1a299e63b26bb239199
sjh3262 <113495560+sjh3262@users.noreply.github.com>
2022-11-23 17:33:05 +0900

    11/23

---
+++ mongodb/atlas/atlas.js
@@ -0,0 +1,28 @@
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "things",
+    "database": "test",
+    "dataSource": "Cluster0",
+    "projection": {
+        "_id": 1
+    }
+});
+
+var config = {
+    method: 'post',
+    url: 'https://data.mongodb-api.com/app/data-tomme/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': 'eM8fMQuZzbbNGpLYvX5tkD48QlhjI84kkaz4Syo9EyblYe7BCTd2FvixEwmSgznU',
+    },
+    data: data
+};
+
+axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
